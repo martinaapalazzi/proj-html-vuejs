deb2124086443c675ad5213f64e8da60d8776212
lpitto86 <luca.p481@gmail.com>
2024-02-01 18:49:47 +0100

    Finalizzazione

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -207,6 +207,8 @@ export default {
 .footer-container {
   height: 520px;
   color: white;
+  margin-top: 5%;
+  border-top: 0.2px solid transparent;
 }
 #footer-main {
   background-image: url("../assets/img/footer-bg.jpg");
